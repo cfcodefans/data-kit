d597fb867214bf4cc25f40dfb7354ce7bb57b1bd
cfcodefans <cf.chenfan@gmail.com>
2019-10-03 16:46:05 +0800

    ..

--- data-hub/src/main/kotlin/cf/data/kit/hub/entities/Pipeline.kt
+++ data-hub/src/main/kotlin/cf/data/kit/hub/entities/Pipeline.kt
@@ -1,18 +1,15 @@
 package cf.data.kit.hub.entities
 
 import java.util.*
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.OneToOne
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "pipelines")
 open class Pipeline : BaseEn() {
-    @OneToOne
+    @ManyToOne
     var src: UrlEn? = null
 
-    @OneToOne
+    @ManyToOne
     var target: UrlEn? = null
 
     @Column(name = "info")
